87fe0c94707662db5192c94ced5ec2a6b7b594c2
Guilherme Rossato <guilhermrossato@gmail.com>
2017-11-05 23:13:03 -0200

    Made the WebGL system more generic
    Also started the validate function

--- WebGL/Script/Application.js
+++ WebGL/Script/Application.js
@@ -1,10 +1,20 @@
 class Application {
+	applyStyle() {
+		var style;
+		style = `margin:0; padding:0;`;
+		document.body.setAttribute("style", style);
+	}
 	constructor() {
 		var canvas = document.getElementById("canvas");
-		var graphics = new WebGLSystem(canvas);
-		graphics.setFragmentSource(document.getElementById("fragScript").text);
+		var graphics = new WebGLSystem({canvas: canvas});
+		graphics.setFragSource(document.getElementById("fragScript").text);
 		graphics.setVertexSource(document.getElementById("vertScript").text);
+		this.graphics = graphics;
+		this.canvas = canvas;
+		this.applyStyle();
 		graphics.init();
 		graphics.render();
+	}
+	onResize() {
 	}
 }
--- WebGL/Script/Classes/WebGLSystem.js
+++ WebGL/Script/Classes/WebGLSystem.js
@@ -1,19 +1,19 @@
 class WebGLSystem {
-	constructor(config) {
+	constructor(config={}) {
 		if (!(config.canvas instanceof HTMLCanvasElement)) {
 			throw new ParameterError("WebGLSystem must receive a valid canvas element in its configuration parameter");
 		}
 		this.canvas = config.canvas;
 		if (config.vertex) {
-			[config.vertex, config.vertex.text, config.vertex.innerText].any((text) => (typeof text === "string" && (this.setVertexSource(text) || true)));
+			[config.vertex, config.vertex.text, config.vertex.innerText].any((text)=>(typeof text === "string" && (this.setVertexSource(text) || true)));
 		}
 		if (config.frag) {
-			[config.frag, config.frag.text, config.frag.innerText].any((text) => (typeof text === "string" && (this.setFragSource(text) || true)));
-		}	
+			[config.frag, config.frag.text, config.frag.innerText].any((text)=>(typeof text === "string" && (this.setFragSource(text) || true)));
+		}
 	}
 	clear(r=0, g=0, b=0, a=1) {
-		this.GL.clearColor(r,g,b,a);
-		this.GL.clear(GL.COLOR_BUFFER_BIT| GL.DEPTH_BUFFER_BIT);
+		this.GL.clearColor(r, g, b, a);
+		this.GL.clear(this.GL.COLOR_BUFFER_BIT | this.GL.DEPTH_BUFFER_BIT);
 	}
 	setVertexSource(source) {
 		this.vertexSource = source;
@@ -24,7 +24,6 @@ class WebGLSystem {
 	_initShaders() {
 		var GL = this.GL;
 		this.shaderProgram = GL.createProgram();
-		var shaderId = this.shaderProgram;
 		var vertId = GL.createShader(GL.VERTEX_SHADER);
 		var fragId = GL.createShader(GL.FRAGMENT_SHADER);
 		GL.shaderSource(vertId, this.vertexSource);
@@ -45,22 +44,85 @@ class WebGLSystem {
 		GL.attachShader(this.shaderProgram, fragId);
 		GL.linkProgram(this.shaderProgram);
 		if (!GL.getProgramParameter(this.shaderProgram, GL.LINK_STATUS)) {
-			var message("Shader Linking Error:\n" + GL.getProgramInfoLog(shader));
+			var message = ("Shader Linking Error:\n" + GL.getProgramInfoLog(shader));
 			throw new Error(message);
 		}
 	}
+	checkUnclosedTags(source) {
+		var line = 0
+		  , j = 0
+		  , stack = []
+		  , c = 'a'
+		  , char_array = source.split('\n').map(line=>line.split(''))
+		  , pairs = [['(', ')'], ['[', ']'], ['{', '}']]
+		  , last = "Undefined Error";
+		for (line = char_array.length - 1; line >= 0; line--) {
+			for (j = char_array[line].length - 1; j >= 0; j--) {
+				c = char_array[line][j];
+				if (pairs.some(pair=>{
+					if (c === pair[1]) {
+						stack.push([pairs[0], line]);
+					} else if (c === pair[0]) {
+						if ((stack.length > 0) && (c !== stack.pop()[0][1])) {
+							last = `${((stack.length == 0)?"Unopened":"Unclosed")} "${pair[1]}" on line ${line}`;
+							return true;
+						}
+					}
+					return false;
+				}
+				)) {
+					return new Error(last);
+				}
+			}
+		}
+		if (stack.length > 0) {
+			return new Error(`Unopened ${stack[0][0][0]} on line ${stack[0][1]}`);
+		}
+		return false;
+	}
 	validateSource(source) {
-		return true;
+		var closedTags = this.checkUnclosedTags(source);
+		if (closedTags) {
+			return closedTags;
+		}
+		return false;
+	}
+	getUniformLocations() {
+		
 	}
 	init() {
 		this.GL = this.canvas.getContext("webgl") || this.canvas.getContext("experimental-webgl");
-		if (!this.GL) { throw new Error("WebGL Context couldn't be created"); }
-		if (typeof this.vertexSource !== "strig") { throw new Error("Missing Vertex Program Source"); }
-		if (typeof this.fragmentSource !== "strig") { throw new Error("Missing Fragment Program Source"); }
+		if (!this.GL) {
+			throw new Error("WebGL Context couldn't be created");
+		}
+		if (typeof this.vertexSource !== "string") {
+			throw new Error("Missing Vertex Program Source");
+		}
+		if (typeof this.fragmentSource !== "string") {
+			throw new Error("Missing Fragment Program Source");
+		}
 		var err;
-		if (err = this.validateSource(this.vertexSource)) { throw new Error("Validation Vertex Source Error:\n"+err); }
-		if (err = this.validateSource(this.fragmentSource)) { throw new Error("Validation Fragment Source Error:\n"+err); }
+		if (err = this.validateSource(this.vertexSource)) {
+			throw new Error("Validation Vertex Source Error:\n" + err);
+		}
+		if (err = this.validateSource(this.fragmentSource)) {
+			throw new Error("Validation Fragment Source Error:\n" + err);
+		}
 		this._initShaders();
+		this.getUniformLocations();
 		this.clear();
+		this.uTime_uniform = this.GL.getUniformLocation(this.shaderProgram, "uTime");
+		this.position_attrib = this.GL.getAttribLocation(this.shaderProgram, "position");
+	}
+	resetTime(x) {
+		this.startTime = x;
+	}
+	updateTime() {
+    	this.GL.uniform1f(this.uTime_uniform, this.startTime-performance.now());
+	}
+	render() {
+		this.GL.useProgram(this.shaderProgram);
+		this.updateTime();
+		this.GL.enableVertexAttribArray(this.position_attrib);
 	}
 }
